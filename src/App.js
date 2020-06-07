e18e1680b8f110791db65a8eceee5fbfa12d1faa
DaeLee <masterkorea01@naver.com>
2020-06-07 16:55:22 +0900

    making my hotel app by myself

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    hotels: [],
+  };
+  getHotels = async () => {
+    const {
+      data: { results },
+    } = await axios.get(
+      "http://www.gimhae.go.kr/openapi/tour/lodging.do?page=5&pageunit=10"
+    );
+    this.setState({ hotels: results, isLoading: false });
+
+    // 데이터 잘 왔나 확인
+    console.log(this.state);
+  };
+  componentDidMount() {
+    this.getHotels();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are Ready"}</div>;
+  }
 }
-
 export default App;
