f7e2a7c4a973b6ac7d1580a271182d08f065989e
DaeLee <masterkorea01@naver.com>
2020-06-07 17:56:24 +0900

    css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import Hotels from "./hotel";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -10,7 +12,7 @@ class App extends React.Component {
     const {
       data: { results },
     } = await axios.get(
-      "http://www.gimhae.go.kr/openapi/tour/lodging.do?page=5&pageunit=10"
+      "http://www.gimhae.go.kr/openapi/tour/lodging.do?page=3&pageunit=10"
     );
     this.setState({ hotels: results, isLoading: false });
 
@@ -22,8 +24,31 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are Ready"}</div>;
+    const { isLoading, hotels } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="hotels">
+            {hotels.map((hotel) => (
+              <Hotels
+                key={hotel.id}
+                id={hotel.id}
+                address={hotel.address}
+                category={hotel.category}
+                name={hotel.name}
+                information={hotel.information}
+                contact={hotel.contact}
+                images={hotel.image}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
 export default App;
---
+++ src/hotel.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Hotel.css";
+
+function Hotels({ id, address, category, name, information, contact, images }) {
+  return (
+    <div className="hotel">
+      <div className="hotel__header">
+        <span className="hotel__name">{name}</span>
+        <span className="hotel__category">{category}</span>
+      </div>
+      <div className="hotel__content">
+        <div className="hotel__information">
+          <span>{information}</span>
+        </div>
+        <div className="hotel__address">
+          <span>{address}</span>
+        </div>
+      </div>
+      <div className="hotel__bottom">
+        <div className="hotell__contact">{contact}</div>
+      </div>
+    </div>
+  );
+}
+
+Hotels.propTypes = {
+  id: PropTypes.number.isRequired,
+  address: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  information: PropTypes.string.isRequired,
+  contact: PropTypes.string.isRequired,
+  latitude: PropTypes.string.isRequired,
+  longitude: PropTypes.string.isRequired,
+  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Hotels;
